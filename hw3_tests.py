73f48678f4aef3ef71eb43b1e94fa4fe61328e92
WannabeDeveloper123 <vishal.murali.kannan@gmail.com>
2025-02-26 13:50:40 -0800

    programming assignment 3 changes

--- hw3_tests.py
+++ hw3_tests.py
@@ -1,7 +1,15 @@
+from functools import reduce
+from idlelib.debugger_r import restart_subprocess_debugger
+from unittest import expectedFailure, removeResult
+
 import data
 import build_data
 import unittest
 
+from hw3 import population_total, population_by_ethnicity, filter_by_state, population_by_education, \
+    population_below_poverty_level, percent_by_education, percent_by_ethnicity, percent_below_poverty_level, \
+    education_greater_than, education_less_than, ethnicity_greater_than, ethnicity_less_than, \
+    below_poverty_level_less_than, below_poverty_level_greater_than
 
 # These two values are defined to support testing below. The
 # data within these structures should not be modified. Doing
@@ -177,32 +185,161 @@ reduced_data = [
 
 class TestCases(unittest.TestCase):
     pass
-
     # Part 1
     # test population_total
+    def test1(self):
+        self.assertEqual(population_total(reduced_data), 655813)
+
+    def test12(self):
+        self.assertEqual(population_total(full_data), 318857056)
+
 
     # Part 2
     # test filter_by_state
+    def test21(self):
+        self.assertEqual(len(filter_by_state(reduced_data, "CA")), 2)
+
+    def test22(self):
+        self.assertEqual(len(filter_by_state(full_data, "AR")), 75)
+
 
     # Part 3
     # test population_by_education
+    def test311(self):
+        result = population_by_education(reduced_data, "Bachelor's Degree or Higher")
+        expected = sum([(i.education["Bachelor's Degree or Higher"] * i.population['2014 Population'])/100 for i in reduced_data])
+        self.assertEqual(expected, result)
+
+    def test312(self):
+        result = population_by_education(reduced_data, "Bachelor's Degree or Higher")
+        expected = sum([(i.education["High School or Higher"] * i.population['2014 Population'])/100 for i in reduced_data])
+        self.assertNotEqual(expected, result)
+
     # test population_by_ethnicity
+    def test321(self):
+        result = population_by_ethnicity(reduced_data, "Black Alone" )
+        expected = sum([(i.ethnicities["Black Alone"] * i.population['2014 Population'])/100 for i in reduced_data])
+        self.assertEqual(expected, result)
+
+
+    def test322(self):
+        result = population_by_ethnicity(reduced_data, "Black Alone" )
+        expected = sum([(i.ethnicities["Asian Alone"] * i.population['2014 Population'])/100 for i in reduced_data])
+        self.assertNotEqual(expected, result)
+
     # test population_below_poverty_level
+    def test331(self):
+        result = population_below_poverty_level(reduced_data)
+        expected = sum([(i.income["Persons Below Poverty Level"] * i.population['2014 Population'])/100 for i in reduced_data])
+        self.assertEqual(expected, result)
+
+    def test332(self):
+        result = population_below_poverty_level(reduced_data)
+        expected = sum([(i.income['Median Household Income'] * i.population['2014 Population'])/100 for i in reduced_data])
+        self.assertNotEqual(expected, result)
+
 
     # Part 4
     # test percent_by_education
+    def test411(self):
+        result = percent_by_education(reduced_data, "High School or Higher")
+        expected = (population_by_education(reduced_data, "High School or Higher")/population_total(reduced_data))*100
+        self.assertEqual(expected, result)
+
+    def test412(self):
+        result = percent_by_education(reduced_data, "High School or Higher")
+        expected = (population_by_education(reduced_data, "Bachelor's Degree or Higher")/population_total(reduced_data))*100
+        self.assertNotEqual(expected, result)
+
+
     # test percent_by_ethnicity
+    def test421(self):
+        result = percent_by_ethnicity(reduced_data, "Asian Alone")
+        expected = (population_by_ethnicity(reduced_data, "Asian Alone")/population_total(reduced_data))*100
+        self.assertEqual(expected, result)
+
+    def test422(self):
+        result = percent_by_ethnicity(reduced_data, "Asian Alone")
+        expected = (population_by_ethnicity(reduced_data, "Black Alone")/population_total(reduced_data))*100
+        self.assertNotEqual(expected, result)
+
     # test percent_below_poverty_level
+    def test431(self):
+        result = percent_below_poverty_level(reduced_data)
+        expected = (population_below_poverty_level(reduced_data)/population_total(reduced_data))*100
+        self.assertEqual(expected, result)
+
+    def test432(self):
+        result = percent_below_poverty_level(reduced_data)
+        expected = (population_below_poverty_level(reduced_data)/population_total(reduced_data))*1000
+        self.assertNotEqual(expected, result)
 
     # Part 5
     # test education_greater_than
+    def test511(self):
+        result = education_greater_than(reduced_data, "Bachelor's Degree or Higher", 2.0)
+        expected = [i for i in reduced_data if "Bachelor's Degree or Higher" in i.education and i.education["Bachelor's Degree or Higher"] > 2.0]
+        self.assertEqual(expected, result)
+
+    def test512(self):
+        result = education_greater_than(reduced_data, "High School or Higher", 2.0)
+        expected = [i for i in reduced_data if "High School or Higher" in i.education and i.education["High School or Higher"] > 2.0]
+        self.assertEqual(expected, result)
+
     # test education_less_than
+    def test521(self):
+        result = education_less_than(reduced_data, "High School or Higher", 3.0)
+        expected = [i for i in reduced_data if "High School or Higher" in i.education and i.education["High School or Higher"] < 3.0]
+        self.assertEqual(expected, result)
+
+    def test522(self):
+        result = education_less_than(reduced_data, "Bachelor's Degree or Higher", 3.0)
+        expected = [i for i in reduced_data if "Bachelor's Degree or Higher" in i.education and i.education["Bachelor's Degree or Higher"] < 3.0]
+        self.assertEqual(expected, result)
+
     # test ethnicity_greater_than
+    def test531(self):
+        result = ethnicity_greater_than(reduced_data, "Black Asian", 2.0)
+        expected = [i for i in reduced_data if "Black Asian" in i.ethnicities and i.ethnicities["Black Asian"] > 2.0]
+        self.assertEqual(expected, result)
+
+    def test532(self):
+        result = ethnicity_greater_than(reduced_data, "White Alone", 2.0)
+        expected = [i for i in reduced_data if "White Alone" in i.ethnicities and i.ethnicities["White Alone"] > 2.0]
+        self.assertEqual(expected, result)
+
     # test ethnicity_less_than
+    def test541(self):
+        result = ethnicity_less_than(reduced_data, "Asian Alone", 49.0)
+        expected = [i for i in reduced_data if "Asian Alone" in i.ethnicities and i.ethnicities["Asian Alone"] < 49.0]
+        self.assertEqual(expected, result)
+
+    def test542(self):
+        result = ethnicity_less_than(reduced_data, "Black Alone", 49.0)
+        expected = [i for i in reduced_data if "Black Alone" in i.ethnicities and i.ethnicities["Black Alone"] < 9.0]
+        self.assertNotEqual(expected, result)
+
     # test below_poverty_level_greater_than
+    def test551(self):
+        result = below_poverty_level_greater_than(reduced_data, 19.9)
+        expected = [i for i in reduced_data if "Persons Below Poverty Level" in i.income and i.income['Persons Below Poverty Level'] > 19.9]
+        self.assertEqual(set(expected), set(result))
+
+    def test552(self):
+        result = below_poverty_level_greater_than(reduced_data, 14.9)
+        expected = [i for i in reduced_data if "Persons Below Poverty Level" in i.income and i.income['Persons Below Poverty Level'] > 4.9]
+        self.assertNotEqual(expected, result)
+
     # test below_poverty_level_less_than
+    def test561(self):
+        result = below_poverty_level_less_than(reduced_data, 13.4)
+        expected = [i for i in reduced_data if "Persons Below Poverty Level" in i.income and i.income['Persons Below Poverty Level'] < 13.4]
+        self.assertEqual(expected, result)
 
-
+    def test562(self):
+        result = below_poverty_level_less_than(reduced_data, 1.4)
+        expected = [i for i in reduced_data if "Persons Below Poverty Level" in i.income and i.income['Persons Below Poverty Level'] < 12.4]
+        self.assertNotEqual(expected, result)
 
 if __name__ == '__main__':
     unittest.main()
